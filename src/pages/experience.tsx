8001a136490dcabca2400cbb0c1c9961c3a842be
andres <anmues97@gmail.com>
2025-07-09 12:09:37 -0700

    WOrk experience

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -14,17 +14,67 @@ const ExperiencePage = () => {
           <Title num={1} title="Work Experience" />
           <div className="mt-8 ml-4">
             <h3 className="text-2xl font-bold text-neon">Cost Analyst</h3>
-            <p className="text-xl text-textDark">Manufacturing Industry Company</p>
-            <p className="text-sm text-gray-400">2021 - 2024</p>
+            <p className="text-xl text-textDark">
+               Solla S.A - <span className="italic">Medellin, Colombia</span>
+            </p>
+            <p className="text-sm text-gray-400 italic">Animal feed manufacturing company (similar to Ritchie-Smith Feeds Inc. in Canada) </p>
+            <p className="text-sm text-gray-400">May 2021 - August 2022</p>
             <ul className="mt-4 ml-5 space-y-2 list-disc text-text">
               <li>
-                Analyzed production costs to identify inefficiencies, achieving a 15% reduction in material waste in the first year.
+                Analyzed raw material and process cost variances, reducing input cost deviations through improved tracking and reporting tools.
               </li>
               <li>
-                Developed and maintained dashboards in Advanced Excel and Power BI for tracking financial KPIs, providing management with visual reports for decision-making.
+                Delivered monthly cost and variance reports that directly supported the plant controller in achieving tighter cost control and operational efficiency.
+              </li>
+                            <li>
+                Collaborated with operations, procurement, and finance teams to standardize cost reporting, enhancing accuracy of fixed and variable cost allocation.
               </li>
               <li>
-                Collaborated with the operations team to model the financial impact of changes in the production line.
+                Streamlined expense and profitability tracking by developing and automating Power BI dashboards.
+              </li>
+            </ul>
+          </div>
+          <div className="mt-8 ml-4">
+            <h3 className="text-2xl font-bold text-neon">Cost Analyst</h3>
+            <p className="text-xl text-textDark">
+               Papelsa S.A (Smurfit Westrock) - <span className="italic">Medellin, Colombia</span>
+            </p>
+            <p className="text-sm text-gray-400 italic">Paper, fiber and packaging manufacturing company, (similar to Cascades in Canada)</p>
+            <p className="text-sm text-gray-400">May 2020 - May 2021 </p>
+            <ul className="mt-4 ml-5 space-y-2 list-disc text-text">
+              <li>
+                Supported plant controlling through effective monthly closings, fixed cost monitoring, and reporting.
+              </li>
+              <li>
+                Ensured accurate SAP reconciliations by coordinating physical inventory counts.
+              </li>
+              <li>
+                Delivered key insights to management by preparing detailed reports on cost variances and product profitability, supporting strategic decision-making.
+              </li>
+              <li>
+                Actively contributed to financial planning by participating in budgeting and forecasting processes.
+              </li>
+              <li>
+                Improved reporting accuracy by leading internal audits on inventory and costing, implementing recommendations.
+              </li>
+            </ul>
+          </div>
+          <div className="mt-8 ml-4">
+            <h3 className="text-2xl font-bold text-neon">Internship Cost Analyst</h3>
+            <p className="text-xl text-textDark">
+               Papelsa S.A (Smurfit Westrock) - <span className="italic">Medellin, Colombia</span>
+            </p>
+            <p className="text-sm text-gray-400 italic">Paper, fiber and packaging manufacturing company, (similar to Cascades in Canada)</p>
+            <p className="text-sm text-gray-400">December 2019 - May 2020 </p>
+            <ul className="mt-4 ml-5 space-y-2 list-disc text-text">
+              <li>
+                Managed audit processes by compiling documentation and testing cost control.
+              </li>
+              <li>
+                Improved data accuracy through participation in inventory counts and SAP reconciliations.
+              </li>
+              <li>
+                Provided analytical support to the Senior Controller by creating reports on cost variances and product profitability.
               </li>
             </ul>
           </div>
@@ -36,7 +86,7 @@ const ExperiencePage = () => {
           <div className="mt-8 ml-4">
             <h3 className="text-2xl font-bold text-neon">Post-Degree Diploma in Data Analytics</h3>
             <p className="text-xl text-textDark">Langara College, Vancouver, BC</p>
-            <p className="text-sm text-gray-400">Expected Graduation: December 2025</p>
+            <p className="text-sm text-gray-400">September 2024 - PRESENT </p>
 
             <h3 className="mt-8 text-2xl font-bold text-neon">Bachelor of Business Administration</h3>
             <p className="text-xl text-textDark">EAFIT University, Medellin, Colombia</p>
